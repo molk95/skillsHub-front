408fa3d3bdae168348294f2414ea33aaa6db92ba
_BenAoun <72893609+YASSCODER@users.noreply.github.com>
2025-05-31 10:42:30 +0100

    apply new layout (#53)

--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: 'logIn', component: SignInComponent },
   { path: 'signUp', component: SignUpComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'reset-password/:token', component: ResetPasswordComponent },
 ];
 
